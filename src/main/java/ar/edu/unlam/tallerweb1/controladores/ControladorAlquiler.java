7466fdd72051f137431f1b015fba1853c80c08b0
IanWagnerLautaro <ianwagnerlautaro@gmail.com>
2021-09-27 21:20:05 -0300

    Creacion de TablaAlquiler
    Mejora del controladorAlquiler agregando la TablaAlquiler

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAlquiler.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAlquiler.java
@@ -1,9 +1,28 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
 import ar.edu.unlam.tallerweb1.modelo.Alquiler;
+import ar.edu.unlam.tallerweb1.repositorio.TablaAlquiler;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+
 
 public class ControladorAlquiler {
-    public Alquiler alquilarAuto(DatosAlquiler datosAlquiler){
-        return new Alquiler();
+
+    private TablaAlquiler tablaAlquiler= TablaAlquiler.getInstance();
+
+    public ModelAndView alquilarAuto(DatosAlquiler datosAlquiler){
+
+        ModelMap model= new ModelMap();
+
+        if(tablaAlquiler.existeAlquilerCon(datosAlquiler.getAuto())){
+
+            return new ModelAndView("ir-alquiler-auto"); //VISTA PLACEHOLDER
+
+        }
+
+        tablaAlquiler.agregar(new Alquiler(datosAlquiler));
+
+        return new ModelAndView("home"); //VISTA PLACEHOLDER
+
     }
 }
--- src/test/java/ar/edu/unlam/tallerweb1/controladores/testControladorAlquiler.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/testControladorAlquiler.java
@@ -3,34 +3,52 @@ package ar.edu.unlam.tallerweb1.controladores;
 import ar.edu.unlam.tallerweb1.modelo.Alquiler;
 import ar.edu.unlam.tallerweb1.modelo.Auto;
 import ar.edu.unlam.tallerweb1.modelo.Cliente;
+import ar.edu.unlam.tallerweb1.repositorio.TablaAlquiler;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class testControladorAlquiler {
+
     Cliente cliente = new Cliente();
     Auto auto = new Auto();
-    ControladorAlquiler controladorAlquiler= new ControladorAlquiler();
-    final String fechaInicio = "10/01/21";
-    final String fechaSalida = "18/01/21";
 
-    DatosAlquiler datosAlquiler = new DatosAlquiler(cliente, auto,fechaInicio, fechaSalida );
+    ControladorAlquiler controladorAlquiler= new ControladorAlquiler();
+
+    private static final String fechaInicio = "10/01/21";
+    private static final String fechaSalida = "18/01/21";
+
+    DatosAlquiler datosAlquiler = new DatosAlquiler(cliente, auto, fechaInicio, fechaSalida );
+
+    private ModelAndView mav;
+
+    @Before
+    public void init() {
+
+        TablaAlquiler.getInstance().reset();
+    }
 
     @Test
     public void queSePuedaAlquilarUnAuto(){
         givenExisteUnAutoYUnCliente();
-        Alquiler alquiler = whenUnClienteAlquilaUnAuto(datosAlquiler);
-        thenElAlquilerEsExitoso(alquiler);
+        whenUnClienteAlquilaUnAuto(datosAlquiler);
+        thenElAlquilerEsExitoso();
     }
 
     private void givenExisteUnAutoYUnCliente() {
     }
 
-    private Alquiler whenUnClienteAlquilaUnAuto(DatosAlquiler datosAlquiler) {
-        return controladorAlquiler.alquilarAuto(datosAlquiler);
+    private void whenUnClienteAlquilaUnAuto(DatosAlquiler datosAlquiler) {
+
+        mav =  controladorAlquiler.alquilarAuto(datosAlquiler);
+
     }
 
-    private void thenElAlquilerEsExitoso(Alquiler alquiler) {
-        assertThat(alquiler).isNotNull();
+    private void thenElAlquilerEsExitoso() {
+
+        assertThat(mav.getViewName()).isEqualTo("home"); //Vista placeholder
     }
 
     ///////////////////////////////////////////
@@ -38,17 +56,21 @@ public class testControladorAlquiler {
     @Test
     public void noPuedeHaberDosAlquileresIguales(){
         givenExisteUnAlquiler(datosAlquiler);
-        Alquiler alquiler = whenUnClienteAlquilaUnAuto(datosAlquiler);
-        thenElAlquilerFalla(alquiler);
+        whenUnClienteAlquilaUnAuto(datosAlquiler);
+        thenElAlquilerFalla();
         
     }
 
-    private Alquiler givenExisteUnAlquiler(DatosAlquiler datosAlquiler) {
-        return controladorAlquiler.alquilarAuto(datosAlquiler);
+    private void givenExisteUnAlquiler(DatosAlquiler datosAlquiler) {
+
+        controladorAlquiler.alquilarAuto(datosAlquiler);
+
     }
 
-    private void thenElAlquilerFalla(Alquiler alquiler) {
-        assertThat(alquiler).isNull();
+    private void thenElAlquilerFalla() {
+
+        assertThat(mav.getViewName()).isEqualTo("ir-alquiler-auto"); //Vista placeholder
+
     }
 
 }
