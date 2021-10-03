df847c7972370d3e885cadc09176dc3bdfa054d5
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-03 20:07:55 -0300

    Se crea la clase del contralador de usuarios administradores. Finaliza la creacion del metodo queUnUsuarioAdministradorPuedaEnviarUnAutoAMantenimeinto(); Se hizo que el test falle a proposito siguiendo las reglas de TDD. Descripcion del metodo: Se espera que dado un usuario administrador y un auto existente, cuando envio dicho auto a mantenimiento, el controlador deberia enviar a la vista de mentenimiento y un mensaje que diga que el auto se envio correctamente a mantenimiento

---
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorUsuarioAdministrador.java
@@ -0,0 +1,10 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import ar.edu.unlam.tallerweb1.modelo.Auto;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ControladorUsuarioAdministrador {
+    public ModelAndView enviarAutoAManteniminento(Auto auto, String fechaInicial) {
+        return null;
+    }
+}
--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorEnviarAutoAMenteniminetoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorEnviarAutoAMenteniminetoTest.java
@@ -1,11 +1,39 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import ar.edu.unlam.tallerweb1.modelo.Auto;
 import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
 
-public class ControladorEnviarAutoAMenteniminetoTest{
+import static org.assertj.core.api.Assertions.*;
+
+public class ControladorEnviarAutoAMenteniminetoTest {
+
+    private static final String FECHA_INICIAL = "3/10/21";
+    private ControladorUsuarioAdministrador controladorUsuarioAdministrador = new ControladorUsuarioAdministrador();
 
     @Test
-    public void queUnUsuarioAdministradorPuedaEnviarUnAutoAMantenimiento(){
+    public void queUnUsuarioAdministradorPuedaEnviarUnAutoAMantenimiento() {
+        givenQueExisteUnUsuarioAdministrador();
+        Auto AUTO = givenQueExisteUnAuto();
 
+        ModelAndView mav = whenEnvioElAutoAMantenimiento(AUTO, FECHA_INICIAL);
+
+        thenElEnvioEsExitoso(mav, "El auto se envio correctamente a mantenimiento");
+    }
+
+    private void givenQueExisteUnUsuarioAdministrador() {
+    }
+
+    private Auto givenQueExisteUnAuto() {
+        return new Auto();
+    }
+
+    private ModelAndView whenEnvioElAutoAMantenimiento(Auto auto, String fechaInicial) {
+        return controladorUsuarioAdministrador.enviarAutoAManteniminento(auto, fechaInicial);
+    }
+
+    private void thenElEnvioEsExitoso(ModelAndView mav, String mensaje) {
+        assertThat(mav.getViewName()).isEqualTo("mantenimiento");
+        assertThat(mav.getModel().get("mensaje")).isEqualTo(mensaje);
     }
 }
