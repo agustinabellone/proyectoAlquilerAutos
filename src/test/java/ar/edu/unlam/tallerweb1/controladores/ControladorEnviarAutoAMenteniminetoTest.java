08430d15c46e5c1fefe755300db45ee3504e509c
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-03 19:42:48 -0300

    Construyendo controlador a partir de TDD. Se crea el metodo queUnUsuarioAdministradorPuedaEnviarUnAutoAMantenimiento();

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorEnviarAutoAMenteniminetoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorEnviarAutoAMenteniminetoTest.java
@@ -1,5 +1,11 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import org.junit.Test;
+
 public class ControladorEnviarAutoAMenteniminetoTest{
 
+    @Test
+    public void queUnUsuarioAdministradorPuedaEnviarUnAutoAMantenimiento(){
+
+    }
 }
