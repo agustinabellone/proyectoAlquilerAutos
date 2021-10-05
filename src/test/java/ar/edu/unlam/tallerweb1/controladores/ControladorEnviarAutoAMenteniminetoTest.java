a4177e2d523d31c5154b1ccc7b136b8d7cad74eb
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-05 07:22:43 -0300

    Se crea el test que no se pueda enviar dos veces un auto al mismo tiempo.

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorEnviarAutoAMenteniminetoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorEnviarAutoAMenteniminetoTest.java
@@ -49,6 +49,11 @@ public class ControladorEnviarAutoAMenteniminetoTest {
     }
 
 
+    @Test
+    public void queNoSePuedaEnviarAlMismoAutoDosVecesAMantenimiento() {
+
+    }
+
     private Auto givenQueExisteUnAutoCon(int kilometrosDefiniddos) {
         auto.setKm(kilometrosDefiniddos);
         return auto;
