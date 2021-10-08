e6e74a67501f2a800ac7078dccbe9446c5226f6d
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-08 11:37:16 -0300

    TDD EN EL SERVICIO DE ENVIAR UN AUTO A MANTENIMIENTO.
    Comienza el test del servicio de envio de un auto a mantenimiento.
    1.- Se comienza con TDD.
    2.- Lo primero que voy a probar es que se pueda enviar un auto a mantenimiento. Y los pasos que deberia seguir para hacer eso.
    3.- Seguro tendre que hacer cambios en la interfaz del servicio que estoy probando dado que no me alcanza solo el auto para realizar este test. Porque ademas deberia tener la fecha y seria mas facil buscar un auto por su patente o su id.

---
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/testServicioEnviarAutoAMantenimiento.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class testServicioEnviarAutoAMantenimiento {
+
+    @Test
+    public void alEnviarUnAutoAMantenimientoYaExistenteElEnvioFalla(){
+
+    }
+}
