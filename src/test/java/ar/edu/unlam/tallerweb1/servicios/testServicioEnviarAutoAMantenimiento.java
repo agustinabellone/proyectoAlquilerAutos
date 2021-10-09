de213ae6efb1a487620263ea7d7a4d61a3dcf9ba
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-09 09:33:07 -0300

    TEST SERVICIO ENVIAR AUTO A MANTENIMIENTO.
    * A medida que fui haciendo el test con TDD me di cuenta que para enviar un auto a mantenimiento necesito ademas de un auto la fecha que lo envio.
    * Entonces el test dice: dado un auto que necesita mantenimiento, cuando envio este al matenimiento, entonces el envio debe ser exitoso y todo la logica que implica que sea exitoso.

--- src/test/java/ar/edu/unlam/tallerweb1/servicios/testServicioEnviarAutoAMantenimiento.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/testServicioEnviarAutoAMantenimiento.java
@@ -1,13 +1,30 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import ar.edu.unlam.tallerweb1.modelo.Auto;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class testServicioEnviarAutoAMantenimiento {
 
-    @Test
-    public void queSePuedaEnviarUnAutoAMantenimiento(){
+    public static final String FECHA_QUE_SE_ENVIA = "09/10/21";
+    private ServicioMantenimiento servicioEnviarAutoAMantenimiento = new ServicioMantenimientoImpl();
 
+    @Test
+    public void queSePuedaEnviarUnAutoAMantenimiento() throws Exception {
+        Auto queNecesitaMantenimiento = givenUnAuto();
+        whenEnvioAMantenimientoAlAuto(queNecesitaMantenimiento, FECHA_QUE_SE_ENVIA);
+        thenElEnvioEsExitoso();
+    }
+
+    private Auto givenUnAuto() {
+        return new Auto();
+    }
+
+    private void whenEnvioAMantenimientoAlAuto(Auto queNecesitaMantenimiento, String FECHA_QUE_SE_ENVIA) throws Exception {
+        servicioEnviarAutoAMantenimiento.enviar(queNecesitaMantenimiento,FECHA_QUE_SE_ENVIA);
+    }
+
+    private void thenElEnvioEsExitoso() {
     }
 }
