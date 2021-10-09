ea03ad70c3fe36abb8a9fe27e64437ae595e1bf2
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-09 11:13:56 -0300

    CAMBIA LA EXCEPTION QUE LANZAN.
    * Segun fui necesitando en el test lanzar excepciones, necesitaba que fueran mas espeicificas por ende el metodo enviar de la interfaz de servicio ademas de enviar el auto a mantenimiento, en caso de que este auto ya este en mantenimiento lanzaria la excepcion de que ya exite ese auto.

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioMantenimiento.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioMantenimiento.java
@@ -1,11 +1,12 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import ar.edu.unlam.tallerweb1.Exceptions.AutoYaExistente;
 import ar.edu.unlam.tallerweb1.modelo.Auto;
 
 
 public interface ServicioMantenimiento {
 
-    Auto enviar(Auto queNecesitaMantenimiento, String fecha_que_se_envia) throws Exception;;
+    Auto enviar(Auto queNecesitaMantenimiento, String fecha_que_se_envia) throws AutoYaExistente;
 
     Auto obtenerPor(String patente);
 }
--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioMantenimientoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioMantenimientoImpl.java
@@ -1,6 +1,9 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import ar.edu.unlam.tallerweb1.Exceptions.AutoYaExistente;
 import ar.edu.unlam.tallerweb1.modelo.Auto;
+import ar.edu.unlam.tallerweb1.repositorio.RepositorioEnviarAutoAMantenimiento;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,9 +12,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ServicioMantenimientoImpl implements ServicioMantenimiento {
 
+    private RepositorioEnviarAutoAMantenimiento repositorioEnviarAutoAMantenimiento;
+
+    @Autowired
+    public ServicioMantenimientoImpl(RepositorioEnviarAutoAMantenimiento repositorioEnviarAutoAMantenimientoImpl) {
+        this.repositorioEnviarAutoAMantenimiento = repositorioEnviarAutoAMantenimientoImpl;
+    }
+
     @Override
-    public Auto enviar(Auto queNecesitaMantenimiento, String fecha_que_se_envia) throws Exception {
+    public Auto enviar(Auto queNecesitaMantenimiento, String fecha_que_se_envia) throws AutoYaExistente {
+        if (existeElAuto(queNecesitaMantenimiento)) {
+            throw new AutoYaExistente();
+        }
         return queNecesitaMantenimiento;
+    }
+
+    private boolean existeElAuto(Auto queNecesitaMantenimiento) {
+        return this.repositorioEnviarAutoAMantenimiento.buscarPor(queNecesitaMantenimiento.getPatente()) != null;
     }
 
     @Override
