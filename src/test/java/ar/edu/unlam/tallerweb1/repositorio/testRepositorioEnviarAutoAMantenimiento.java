4821bc3a3033c83115c9d9a73cc2b6bf1e60a0c2
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-12 11:12:58 -0300

    SE CREA EL PRIMER TEST.
    Se crea un caso de uso el cual prueba que se guarda correctamente un auto en mantenimiento en la base de datos.

--- src/test/java/ar/edu/unlam/tallerweb1/repositorio/testRepositorioEnviarAutoAMantenimiento.java
+++ src/test/java/ar/edu/unlam/tallerweb1/repositorio/testRepositorioEnviarAutoAMantenimiento.java
@@ -1,4 +1,11 @@
 package ar.edu.unlam.tallerweb1.repositorio;
 
+import org.junit.Test;
+
 public class testRepositorioEnviarAutoAMantenimiento {
+
+    @Test
+    public void queSeGuardeUnAutoEnMantenimientoCorrectamente() {
+
+    }
 }
