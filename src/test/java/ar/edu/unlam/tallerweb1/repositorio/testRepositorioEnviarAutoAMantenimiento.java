67eb490a569b2023d851d7487087af8aeb7f77b2
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-12 11:48:21 -0300

    Cambio la ubicacion de la clase SpringTest ya que estaba dentro de la carpeta java y no dentro del package tallerweb1, por lo tanto al importarlo no lo encontraba

--- src/test/java/ar/edu/unlam/tallerweb1/repositorio/testRepositorioEnviarAutoAMantenimiento.java
+++ src/test/java/ar/edu/unlam/tallerweb1/repositorio/testRepositorioEnviarAutoAMantenimiento.java
@@ -1,8 +1,13 @@
 package ar.edu.unlam.tallerweb1.repositorio;
 
+import ar.edu.unlam.tallerweb1.SpringTest;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class testRepositorioEnviarAutoAMantenimiento {
+public class testRepositorioEnviarAutoAMantenimiento extends SpringTest {
+
+    @Autowired
+    private RepositorioEnviarAutoAMantenimiento repositorioEnviarAutoAMantenimiento;
 
     @Test
     public void queSeGuardeUnAutoEnMantenimientoCorrectamente() {
