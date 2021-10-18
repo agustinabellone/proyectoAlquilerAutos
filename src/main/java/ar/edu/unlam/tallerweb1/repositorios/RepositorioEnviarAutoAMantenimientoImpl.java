6f486c5da5ce33e312cfc5434629cd774b3605b9
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-18 09:08:18 -0300

    PRIMER CASO DE USO EXITOSO. Se realizo correctamente el testque busca por patente.

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioEnviarAutoAMantenimientoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioEnviarAutoAMantenimientoImpl.java
@@ -2,14 +2,29 @@ package ar.edu.unlam.tallerweb1.repositorios;
 
 
 import ar.edu.unlam.tallerweb1.modelo.Auto;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RepositorioEnviarAutoAMantenimientoImpl implements RepositorioEnviarAutoAMantenimiento {
 
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public RepositorioEnviarAutoAMantenimientoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Auto buscarPor(String patente) {
-        return null;
+        final Session session = sessionFactory.getCurrentSession();
+        //session.createSQLQuery("SELECT * FROM ");
+        return (Auto) session.createCriteria(Auto.class)
+                .add(Restrictions.eq("patente", patente))
+                .uniqueResult();
     }
 
     @Override
--- src/test/java/ar/edu/unlam/tallerweb1/repositorios/testRepositorioEnviarAutoAMantenimiento.java
+++ src/test/java/ar/edu/unlam/tallerweb1/repositorios/testRepositorioEnviarAutoAMantenimiento.java
@@ -1,9 +1,42 @@
 package ar.edu.unlam.tallerweb1.repositorios;
 
+import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Auto;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 
-public class testRepositorioEnviarAutoAMantenimiento {
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class testRepositorioEnviarAutoAMantenimiento extends SpringTest {
+
+    public static final String PATENTE = "ABC123";
+
+    @Autowired
+    private RepositorioEnviarAutoAMantenimiento repositorioEnviarAutoAMantenimiento;
 
     @Test
-    public void queSePuedaBuscarPorPatente(){}
+    @Rollback
+    @Transactional
+    public void queSePuedaBuscarPorPatente() {
+        givenExisteUnAutoConPatente(PATENTE);
+        Auto buscado = whenBuscoUnAutoConPatente(PATENTE);
+        thenEncuentroEl(buscado);
+    }
+
+    private void givenExisteUnAutoConPatente(String patente) {
+        Auto buscado = new Auto();
+        buscado.setPatente(patente);
+        session().save(buscado);
+    }
+
+    private Auto whenBuscoUnAutoConPatente(String patente) {
+        return repositorioEnviarAutoAMantenimiento.buscarPor(patente);
+    }
+
+    private void thenEncuentroEl(Auto buscado) {
+        assertThat(buscado).isNotNull();
+    }
 }
