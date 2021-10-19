56dcc78ca1d5642e4996a5cd422b806ff6001782
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-19 07:55:50 -0300

    CREO EL TEST DE PERSISTENCIA DE ENVIAR UN AUTO A MANTENIMIENTO. Mediante TDD construire este test de persistencia.

---
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/testPersistenciaEnviarAutoAMantenimiento.java
@@ -0,0 +1,22 @@
+package ar.edu.unlam.tallerweb1.persistencia;
+
+import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioEnviarAutoAMantenimiento;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+public class testPersistenciaEnviarAutoAMantenimiento extends SpringTest {
+    //insertar, modificar, eliminar
+    @Autowired
+    private RepositorioEnviarAutoAMantenimiento repositorioEnviarAutoAMantenimiento;
+
+    @Test
+    @Rollback
+    @Transactional
+    public void queSePuedaGuardarUnAutoEnMantenimiento() {
+
+    }
+}
