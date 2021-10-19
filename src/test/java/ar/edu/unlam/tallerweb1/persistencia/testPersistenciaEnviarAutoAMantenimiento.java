60a70701d8b15a9571ce514691aa593a017c3791
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-19 09:22:00 -0300

    El test que guarda un auto es exitoso

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/testPersistenciaEnviarAutoAMantenimiento.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/testPersistenciaEnviarAutoAMantenimiento.java
@@ -1,12 +1,14 @@
 package ar.edu.unlam.tallerweb1.persistencia;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Auto;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioEnviarAutoAMantenimiento;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import static org.assertj.core.api.Assertions.*;
 
 public class testPersistenciaEnviarAutoAMantenimiento extends SpringTest {
     //insertar, modificar, eliminar
@@ -17,6 +19,22 @@ public class testPersistenciaEnviarAutoAMantenimiento extends SpringTest {
     @Rollback
     @Transactional
     public void queSePuedaGuardarUnAuto() {
+        Auto existente = givenExisteUnAuto();
+        Long idDelAuto = whenGuardoElAuto(existente);
+        thenPuedoBuscarloPorId(idDelAuto);
+    }
+
+    private Auto givenExisteUnAuto() {
+        return new Auto();
+    }
+
+    private Long whenGuardoElAuto(Auto existente) {
+        session().save(existente);
+        return existente.getId();
+    }
+
+    private void thenPuedoBuscarloPorId(Long idDelAuto) {
+        assertThat(session().get(Auto.class,idDelAuto)).isNotNull();
     }
 
     @Test
