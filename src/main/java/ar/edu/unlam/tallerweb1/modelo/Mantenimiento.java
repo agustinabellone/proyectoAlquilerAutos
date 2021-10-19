4d9501e869419a777107d7318c8269140aa0be4e
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-19 09:31:13 -0300

    El test que se puede guardar un auto en mantenimiento, ademas se creo la entidad mantenimiento

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Mantenimiento.java
@@ -0,0 +1,21 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Mantenimiento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/testPersistenciaEnviarAutoAMantenimiento.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/testPersistenciaEnviarAutoAMantenimiento.java
@@ -2,19 +2,16 @@ package ar.edu.unlam.tallerweb1.persistencia;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
 import ar.edu.unlam.tallerweb1.modelo.Auto;
-import ar.edu.unlam.tallerweb1.repositorios.RepositorioEnviarAutoAMantenimiento;
+import ar.edu.unlam.tallerweb1.modelo.Mantenimiento;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class testPersistenciaEnviarAutoAMantenimiento extends SpringTest {
     //insertar, modificar, eliminar
-    @Autowired
-    private RepositorioEnviarAutoAMantenimiento repositorioEnviarAutoAMantenimiento;
-
     @Test
     @Rollback
     @Transactional
@@ -34,13 +31,29 @@ public class testPersistenciaEnviarAutoAMantenimiento extends SpringTest {
     }
 
     private void thenPuedoBuscarloPorId(Long idDelAuto) {
-        assertThat(session().get(Auto.class,idDelAuto)).isNotNull();
+        assertThat(session().get(Auto.class, idDelAuto)).isNotNull();
     }
 
     @Test
     @Rollback
     @Transactional
     public void queSePuedaGuardarUnMatenimiento() {
+        Mantenimiento existente = givenExisteUnMantenimiento();
+        Long idDelMantenimiento = whenGuardoUnMantenimiento(existente);
+        thenLoPuedoBuscarPorId(idDelMantenimiento);
+    }
+
+    private Mantenimiento givenExisteUnMantenimiento() {
+        return new Mantenimiento();
+    }
+
+    private Long whenGuardoUnMantenimiento(Mantenimiento existente) {
+        session().save(existente);
+        return existente.getId();
+    }
+
+    private void thenLoPuedoBuscarPorId(Long idDelMantenimiento) {
+        assertThat(session().get(Mantenimiento.class, idDelMantenimiento)).isNotNull();
     }
 
     @Test
