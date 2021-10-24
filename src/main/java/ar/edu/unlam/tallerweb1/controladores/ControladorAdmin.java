3595d68ff4fe1693ae9d1ad59b6c1bfd9cb243b6
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-24 10:39:10 -0300

    REALIZACION DEL TEST DEL CONTROLADOR DEL ADMIN Se realizo correctamente el test del controlador del administrador. Lo que se testeo fue que cuando un usuario con rol de administrador intente acceder al penal principal lo pueda hacer. En cambio un usuario con otro rol que no sea administrador no pueda ingresar. Para este caso lo realice con tdd mockee la request y la session, ademas esa request se la paso al metodo del controlador que es quien recibe la request por parametro y valida que el atriuto que le llega por HttpSession es igual a administrador.

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdmin.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdmin.java
@@ -1,15 +1,16 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
 public class ControladorAdmin {
 
-    @RequestMapping(method = RequestMethod.GET, path = "ir-a-panel-principal")
-    public ModelAndView irAlPanelPrincipal() {
-        return new ModelAndView("panel-principal");
+    public ModelAndView irAlPanelPrincipal(HttpServletRequest request) {
+        if (request.getSession().getAttribute("rol").equals("ADMIN")) {
+            return new ModelAndView("panel-principal");
+        }
+        return new ModelAndView("home");
     }
+
 }
---
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/testControladorAdmin.java
@@ -0,0 +1,54 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class testControladorAdmin {
+    private static final String ADMIN = "ADMIN";
+    private static final String INVITADO = "INVITADO";
+    private HttpServletRequest request = mock(HttpServletRequest.class);
+    private HttpSession httpSession = mock(HttpSession.class);
+    private ModelAndView modelAndView;
+    private ControladorAdmin controlador = new ControladorAdmin();
+
+    @Test
+    public void queUnUsuarioAdministradorPuedaIngresarAlPanelPrincipal() {
+        givenUnUsuarioConRol(ADMIN);
+        whenAccedeAlPanelPrincipal(ADMIN);
+        thenAccedeAlPanelPrincipal(this.modelAndView);
+    }
+
+    private void givenUnUsuarioConRol(String rol) {
+    }
+
+    private void whenAccedeAlPanelPrincipal(String rolDelUsuario) {
+        when(request.getSession()).thenReturn(httpSession);
+        when(request.getSession().getAttribute(anyString())).thenReturn(rolDelUsuario);
+        this.modelAndView = controlador.irAlPanelPrincipal(request);
+    }
+
+    private void thenAccedeAlPanelPrincipal(ModelAndView modelAndView) {
+        assertThat(modelAndView.getViewName()).isEqualTo("panel-principal");
+        assertThat(request.getSession().getAttribute("rol")).isEqualTo(ADMIN);
+    }
+
+    @Test
+    public void queUnUsuarioSinRolDeAdministradorNoPuedoAccederAlPanelPrincipal() {
+        givenUnUsuarioConRol(INVITADO);
+        whenAccedeAlPanelPrincipal(INVITADO);
+        thenNoAccedeAlPanelPrincipal(this.modelAndView);
+    }
+
+    private void thenNoAccedeAlPanelPrincipal(ModelAndView modelAndView) {
+        assertThat(modelAndView.getViewName()).isEqualTo("home");
+        assertThat(request.getSession().getAttribute("rol")).isEqualTo(INVITADO);
+    }
+}
