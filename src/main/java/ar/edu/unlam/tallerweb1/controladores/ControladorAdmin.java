7d8b881b867e4083f91a6bc51f36227a5aa7cc5a
Ezequiel <aibarhernanezequiel@gmail.com>
2021-10-24 10:44:24 -0300

    Se agrega el @RequestMapping que es la forma de como va acceder a este metodo

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdmin.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdmin.java
@@ -2,10 +2,13 @@ package ar.edu.unlam.tallerweb1.controladores;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 public class ControladorAdmin {
 
+    @RequestMapping(method = RequestMethod.GET, path = "/administrador")
     public ModelAndView irAlPanelPrincipal(HttpServletRequest request) {
         if (request.getSession().getAttribute("rol").equals("ADMIN")) {
             return new ModelAndView("panel-principal");
