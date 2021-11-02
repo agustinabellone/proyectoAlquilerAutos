79bef200eee7f0b0bd22db841cee5b23b22dde7f
Ezequiel <aibarhernanezequiel@gmail.com>
2021-11-02 08:13:50 -0300

    Se realizo el metodo lo mas simple posible y ademas no utilizo la clase ModelMap ya que con los metodos del ModelAndView no es necesario

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdministrador.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdministrador.java
@@ -1,15 +1,31 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ControladorAdministrador {
 
-    public ModelAndView irALaVistaPrincipal() {
-        ModelMap model = new ModelMap();
-        model.put("error","No tienes los permisos necesarios");
-        return new ModelAndView("redirect:/login",model);
+    private ModelAndView modelAndView = new ModelAndView();
+
+    public ModelAndView irALaVistaPrincipal(HttpServletRequest request) {
+        if (elRolEstaSeteado(request) && elRolEsAdministrador(request)){
+            modelAndView.addObject("mensaje","Bienvenido!!!");
+            modelAndView.setViewName("redirect:/panel-principal");
+        } else{
+            modelAndView.addObject("error","No tienes los permisos necesarios");
+            modelAndView.setViewName("redirect:/login");
+        }
+        return modelAndView;
+    }
+
+    private boolean elRolEstaSeteado(HttpServletRequest request) {
+        return request.getSession().getAttribute("rol") != null;
+    }
+
+    private boolean elRolEsAdministrador(HttpServletRequest request) {
+        return request.getSession().getAttribute("rol").equals("admin");
     }
 }
