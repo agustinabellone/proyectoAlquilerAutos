ace14a0f80d4b5ffe289cd893c3b6e53e5383f68
ezequiel <ezequiel@Debian.168.1.11>
2021-11-09 07:35:33 -0300

    Test del repositortio del administrador.
    En este caso se va a probar que se puedan guardar autos alquilados, para luego poder buscarlos por su situacion.

---
+++ src/test/java/ar/edu/unlam/tallerweb1/repositorios/testRepositorioAdministrador.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import ar.edu.unlam.tallerweb1.SpringTest;
+import org.junit.Test;
+
+public class testRepositorioAdministrador extends SpringTest {
+
+    @Test
+    public void queSePuedaGuardarAutosAlquiladosParaQuePuedanSerObtenidos() {
+    }
+}
