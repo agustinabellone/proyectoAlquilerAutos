739a8f241e2f43ba8db192f7adcb5636793df8cc
Aibar Hernan Ezequiel <aibarhernanezequiel@gmail.com>
2021-11-24 17:59:54 -0300

    Testea que se pueda obtener una lista con empleados encargados de la devolucion

---
+++ src/test/java/ar/edu/unlam/tallerweb1/repositorios/TestRepositorioAdministradorSeccionEmpleado.java
@@ -0,0 +1,44 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class TestRepositorioAdministradorSeccionEmpleado extends SpringTest {
+
+    @Autowired
+    private RepositorioUsuario repositorioUsuario;
+    private static final String ENCARGADO = "encargado";
+
+    @Test
+    @Rollback
+    @Transactional
+    public void queSePuedaObtenerUnEmpleadoEncargadoDeDevolucion() {
+        givenExisteUnEmpleado(ENCARGADO, 5);
+        List<Usuario> usuarioList = whenBuscoPorRolDe(ENCARGADO);
+        thenObtengoUnaListaDeTodosLosEncargados(usuarioList, 5);
+    }
+
+    private void givenExisteUnEmpleado(String rol, int cantidad) {
+        for (int i = 0; i < cantidad; i++) {
+            Usuario usuario = new Usuario();
+            usuario.setRol(rol);
+            session().save(usuario);
+        }
+    }
+
+    private List<Usuario> whenBuscoPorRolDe(String rol) {
+        return repositorioUsuario.buscarUsuariosPorRol(rol);
+    }
+
+    private void thenObtengoUnaListaDeTodosLosEncargados(List<Usuario> usuarioList, int cantidad_esperada) {
+        assertThat(usuarioList).hasSize(cantidad_esperada);
+    }
+}
