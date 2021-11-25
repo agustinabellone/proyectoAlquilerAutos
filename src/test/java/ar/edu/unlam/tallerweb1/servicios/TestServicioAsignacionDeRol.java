c19597a7d6b81eee73f9a8b0d8821bc164f5fd87
Aibar Hernan Ezequiel <aibarhernanezequiel@gmail.com>
2021-11-24 21:57:45 -0300

    Crea el tes del servicio de la asignacion de rol

---
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/TestServicioAsignacionDeRol.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import org.junit.Test;
+
+public class TestServicioAsignacionDeRol {
+
+    private ServicioUsuario servicioUsuario = new ServicioUsuarioImpl();
+
+    @Test
+    public void lanzarUnaExceptionSiNoHayUsuariosPendientesDeRol() {
+    }
+}
